986002716d9dffdfecce5a0bb9953692a650770c
Patrick Venture <venture@google.com>
2018-08-01 11:45:49 -0700

    flash-ipmi: start implementing FlashStartTransfer
    Change-Id: I1f8b1498d517c0661e98b1ba895e7152f7a9ed8e
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ ipmi.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "host-ipmid/ipmid-api.h"
+
+#include "flash-ipmi.hpp"
+
+/**
+ * Prepare to receive a BMC image and then a signature.
+ *
+ * @param[in] reqBuf - the IPMI packet.
+ * @param[in] replyBuf - Pointer to buffer for any response.
+ * @param[in,out] dataLen - Initially reqBuf length, set to replyBuf
+ * length when done.
+ * @return corresponding IPMI return code.
+ */
+ipmi_ret_t startTransfer(UpdateInterface* updater, const uint8_t* reqBuf,
+                         uint8_t* replyBuf, size_t* dataLen);
