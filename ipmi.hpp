79e131fce6e308f7e0f96bc71b07a7f0ebeeffcd
Patrick Venture <venture@google.com>
2018-08-01 13:34:35 -0700

    ipmi: start implementing flashDataBlock
    Change-Id: I780c0e5da77a027ce23a674bd3fdbf08b8a81c14
    Signed-off-by: Patrick Venture <venture@google.com>

--- ipmi.hpp
+++ ipmi.hpp
@@ -16,3 +16,16 @@
  */
 ipmi_ret_t startTransfer(UpdateInterface* updater, const uint8_t* reqBuf,
                          uint8_t* replyBuf, size_t* dataLen);
+
+/**
+ * Receive a flash image data block and store it.
+ *
+ * @param[in] updater - Pointer to Updater object.
+ * @param[in] reqBuf - the IPMI packet.
+ * @param[in] replyBuf - Pointer to buffer for any response.
+ * @param[in,out] dataLen - Initially reqBuf length, set to replyBuf
+ * length when done.
+ * @return corresponding IPMI return code.
+ */
+ipmi_ret_t dataBlock(UpdateInterface* updater, const uint8_t* reqBuf,
+                     uint8_t* replyBuf, size_t* dataLen);
