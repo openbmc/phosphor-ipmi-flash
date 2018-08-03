8d9f7322f46afaab81e32d1999f5bc000f1ba9a6
Patrick Venture <venture@google.com>
2018-08-03 10:39:13 -0700

    ipmi: start implementing flashStartHash
    Change-Id: I5465bd672eac510be30d50cac8cf34b86a24ca40
    Signed-off-by: Patrick Venture <venture@google.com>

--- test/updater_mock.hpp
+++ test/updater_mock.hpp
@@ -13,4 +13,5 @@ class UpdaterMock : public UpdateInterface
     MOCK_METHOD1(start, bool(uint32_t));
     MOCK_METHOD2(flashData, bool(uint32_t, const std::vector<uint8_t>&));
     MOCK_METHOD0(flashFinish, bool());
+    MOCK_METHOD1(startHash, bool(uint32_t));
 };
