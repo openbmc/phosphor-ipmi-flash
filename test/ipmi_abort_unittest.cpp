8a4dde96b6ba606664ee7156edb6c9962a8a3ee2
Patrick Venture <venture@google.com>
2018-08-03 18:31:01 -0700

    ipmi: start implementing flashAbort
    Change-Id: I1a05ba7e9323046a09b8582351be987cdaab5a15
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ test/ipmi_abort_unittest.cpp
@@ -0,0 +1,32 @@
+#include "flash-ipmi.hpp"
+#include "ipmi.hpp"
+
+#include "updater_mock.hpp"
+
+#include <gtest/gtest.h>
+
+using ::testing::Return;
+using ::testing::StrictMock;
+
+// ipmid.hpp isn't installed where we can grab it and this value is per BMC
+// SoC.
+#define MAX_IPMI_BUFFER 64
+
+TEST(IpmiAbortTest, CallPassedOn)
+{
+    // The abort call is another pass-through command.
+
+    StrictMock<UpdaterMock> updater;
+
+    size_t dataLen;
+    uint8_t request[MAX_IPMI_BUFFER] = {0};
+    uint8_t reply[MAX_IPMI_BUFFER] = {0};
+
+    dataLen = 1; // request is only the command.
+    request[0] = FlashSubCmds::flashAbort;
+
+    EXPECT_CALL(updater, abortUpdate()).WillOnce(Return(true));
+    EXPECT_EQ(IPMI_CC_OK, abortUpdate(&updater, request, reply, &dataLen));
+    EXPECT_EQ(sizeof(uint8_t), dataLen);
+    EXPECT_EQ(0, reply[0]);
+}
