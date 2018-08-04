671850b2684308f73c514581b2e92ceadbef56f9
Patrick Venture <venture@google.com>
2018-08-03 18:22:09 -0700

    ipmi: start implementing flashDataVerify
    Change-Id: I446ca8573db31eb53dc7aefbe25cdb9d23d51d29
    Signed-off-by: Patrick Venture <venture@google.com>

--- ipmi.cpp
+++ ipmi.cpp
@@ -30,6 +30,7 @@ IpmiFlashHandler getCommandHandler(FlashSubCmds command)
             {FlashSubCmds::flashStartHash, startHash},
             {FlashSubCmds::flashHashData, hashBlock},
             {FlashSubCmds::flashHashFinish, hashFinish},
+            {FlashSubCmds::flashDataVerify, dataVerify},
         };
 
     auto results = subHandlers.find(command);
@@ -168,6 +169,20 @@ ipmi_ret_t hashFinish(UpdateInterface* updater, const uint8_t* reqBuf,
                       uint8_t* replyBuf, size_t* dataLen)
 {
     if (!updater->hashFinish())
+    {
+        return IPMI_CC_INVALID;
+    }
+
+    /* We were successful and set the response byte to 0. */
+    replyBuf[0] = 0x00;
+    (*dataLen) = 1;
+    return IPMI_CC_OK;
+}
+
+ipmi_ret_t dataVerify(UpdateInterface* updater, const uint8_t* reqBuf,
+                      uint8_t* replyBuf, size_t* dataLen)
+{
+    if (!updater->startDataVerification())
     {
         return IPMI_CC_INVALID;
     }
--- ipmi.hpp
+++ ipmi.hpp
@@ -104,3 +104,16 @@ ipmi_ret_t hashBlock(UpdateInterface* updater, const uint8_t* reqBuf,
  */
 ipmi_ret_t hashFinish(UpdateInterface* updater, const uint8_t* reqBuf,
                       uint8_t* replyBuf, size_t* dataLen);
+
+/**
+ * Start the flash image verification process (whatever that is).
+ *
+ * @param[in] updater - Pointer to Updater object.
+ * @param[in] reqBuf - the IPMI packet.
+ * @param[in] replyBuf - Pointer to buffer for any response.
+ * @param[in,out] dataLen - Initially reqBuf length, set to replyBuf
+ * length when done.
+ * @return corresponding IPMI return code.
+ */
+ipmi_ret_t dataVerify(UpdateInterface* updater, const uint8_t* reqBuf,
+                      uint8_t* replyBuf, size_t* dataLen);
