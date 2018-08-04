3274a065eb447abee25e0d2b1c7d6450950a804d
Patrick Venture <venture@google.com>
2018-08-03 17:23:57 -0700

    main: cleanup command handling
    Change-Id: I7b07bb13948607b40365411c16510073d33ba57b
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ test/ipmi_command_unittest.cpp
@@ -0,0 +1,42 @@
+#include "ipmi.hpp"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+void EqualFunctions(IpmiFlashHandler lhs, IpmiFlashHandler rhs)
+{
+    EXPECT_FALSE(lhs == nullptr);
+    EXPECT_FALSE(rhs == nullptr);
+    ipmi_ret_t (*const* lPtr)(UpdateInterface*, const uint8_t*, uint8_t*,
+                              size_t*) =
+        lhs.target<ipmi_ret_t (*)(UpdateInterface*, const uint8_t*, uint8_t*,
+                                  size_t*)>();
+    ipmi_ret_t (*const* rPtr)(UpdateInterface*, const uint8_t*, uint8_t*,
+                              size_t*) =
+        rhs.target<ipmi_ret_t (*)(UpdateInterface*, const uint8_t*, uint8_t*,
+                                  size_t*)>();
+    EXPECT_TRUE(lPtr);
+    EXPECT_TRUE(rPtr);
+    EXPECT_EQ(*lPtr, *rPtr);
+    return;
+}
+}
+
+TEST(IpmiCommandTest, VerifyCommandReturnsExpected)
+{
+    // Given a subcommand that's valid, make sure it returns the expected
+    // pointer.
+
+    auto result = getCommandHandler(FlashSubCmds::flashHashFinish);
+    EqualFunctions(hashFinish, result);
+}
+
+TEST(IpmiCommandTest, VerifyInvalidCommandReturnsNull)
+{
+    // Given a subcommand that's invalid, make sure it returns the nullptr.
+
+    auto result = getCommandHandler(static_cast<FlashSubCmds>(25));
+    EXPECT_EQ(result, nullptr);
+}
