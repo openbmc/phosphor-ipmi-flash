f5e53511575a4aa493ff980b1e2ccd140c57e2c9
Patrick Venture <venture@google.com>
2018-08-08 10:30:57 -0700

    tools: start updater main execution
    Change-Id: I89e46ffd361fbc8c44b14012be793b274b6c3ebb
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ tools/updater.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "updater.hpp"
+
+#include <set>
+#include <stdexcept>
+
+extern "C" {
+//#include "ipmitool/ipmi_intf.h"
+
+// This struct is defind in src/plugins/open/open.c
+extern struct ipmi_intf ipmi_open_intf;
+} // extern "C"
+
+void UpdaterMain(const std::string& interface, const std::string& image,
+                 const std::string& signature)
+{
+    static const std::set<std::string> supportedInterfaces = {"ipmibt"};
+
+    /* Check if interface is supported. */
+    if (!supportedInterfaces.count(interface))
+    {
+        throw std::runtime_error("Unsupported interface");
+    }
+
+    return;
+}
