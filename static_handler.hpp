18b13b7c5ec3f83de553523c589812b172fb3629
Patrick Venture <venture@google.com>
2018-11-15 09:31:51 -0800

    static_handler: implement basic version
    Implement basic version that simply caches the information written to
    it.  Later, error conditions will need addressing.
    
    Change-Id: Ife27b72a6a685e500a0a16c55975c9f4469b206a
    Signed-off-by: Patrick Venture <venture@google.com>

--- static_handler.hpp
+++ static_handler.hpp
@@ -3,6 +3,7 @@
 #include "image_handler.hpp"
 
 #include <cstdint>
+#include <fstream>
 #include <memory>
 #include <string>
 #include <vector>
@@ -14,11 +15,19 @@ class StaticLayoutHandler : public ImageHandlerInterface
 {
   public:
     /**
-     * Create a StaticLayoutHandler.
+     * Create a StaticLayoutHandler.  This appears to be a basic file writer, if
+     * it doesn't end up doing more, then it will be converted into such.
+     *
+     * @param[in] temporaryName - where to write the firmware image while it's
+     * being uploaded.
      */
     explicit StaticLayoutHandler(const std::string& temporaryName) :
         stagedFilename(temporaryName){};
 
+    /* Destroying the ofstream closes it first. */
+    ~StaticLayoutHandler() = default;
+
+    /* Opens the stagedFilename. */
     bool open(const std::string& path) override;
     void close() override;
     bool write(std::uint32_t offset,
@@ -26,6 +35,9 @@ class StaticLayoutHandler : public ImageHandlerInterface
 
   private:
     std::string path;
+
+    /** The staged output file stream object. */
+    std::ofstream stagedOutput;
 
     /** The file to use for staging the bytes. */
     std::string stagedFilename;
