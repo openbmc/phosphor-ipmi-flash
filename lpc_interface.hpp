971d2e21ceedb49c1f2c127843ed1fd722f974d2
Patrick Venture <venture@google.com>
2018-11-14 20:16:33 -0800

    add lpc mapping interface and stubs
    Add an lpc mapping interface to allow for multiple mapping
    implementations, such as one for Aspeed and one for Nuvoton.
    
    Add empty implementations for Aspeed and Nuvoton.
    Add configuration options to enable aspeed or nuvoton lpc.
    
    Change-Id: I3fd2b1e437db6366c7656f294d138224c25d4e81
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ lpc_interface.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdint>
+#include <utility>
+
+namespace blobs
+{
+
+/**
+ * Different LPC memory map implementations may require different mechanisms for
+ * specific tasks such as mapping the memory window.
+ */
+class LpcMapperInterface
+{
+  public:
+    virtual ~LpcMapperInterface() = default;
+
+    /**
+     * Returns true if requested window was mapped. If the length requested
+     * is too large, windowSize will be written with the max size that the
+     * BMC can map and returns false.
+     *
+     * @param[in] address - The address for mapping (passed to LPC window)
+     * @param[in] length - The length of the region
+     * @return pair<windowOffset, windowSize) - The offset into the window and
+     * length of the region.  On failure, length is set to 0.
+     */
+    virtual std::pair<std::uint32_t, std::uint32_t>
+        mapWindow(std::uint32_t address, std::uint32_t length) = 0;
+};
+
+} // namespace blobs
---
+++ lpc_nuvoton.cpp
@@ -0,0 +1,24 @@
+#include "lpc_nuvoton.hpp"
+
+#include "lpc_interface.hpp"
+
+#include <cstdint>
+#include <memory>
+#include <utility>
+
+namespace blobs
+{
+
+std::unique_ptr<LpcMapperInterface> LpcMapperNuvoton::createNuvotonMapper()
+{
+    /* NOTE: Considered making one factory for both types. */
+    return std::make_unique<LpcMapperNuvoton>();
+}
+
+std::pair<std::uint32_t, std::uint32_t>
+    LpcMapperNuvoton::mapWindow(std::uint32_t address, std::uint32_t length)
+{
+    return false;
+}
+
+} // namespace blobs
