d9a3f826b65aee425951998c5fba1cc352974615
Patrick Venture <venture@google.com>
2018-11-14 20:16:33 -0800

    add lpc mapping interface and stubs
    Add an lpc mapping interface to allow for multiple mapping
    implementations, such as one for Aspeed and one for Nuvoton.
    
    Add empty implementations for Aspeed and Nuvoton.
    Add configuration options to enable aspeed or nuvoton lpc.
    
    Change-Id: I3fd2b1e437db6366c7656f294d138224c25d4e81
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ lpc_aspeed.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "lpc_interface.hpp"
+
+namespace blobs
+{
+
+class LpcMapperAspeed : public LpcMapperInterface
+{
+  public:
+    static std::unique_ptr<LpcMapperInterface> createAspeedMapper();
+
+    /* TODO: Needs reserved memory region's physical address and size. */
+    LpcMapperAspeed() = default;
+
+    std::pair<std::uint32_t, std::uint32_t>
+        mapWindow(std::uint32_t address, std::uint32_t length) override;
+};
+
+} // namespace blobs
