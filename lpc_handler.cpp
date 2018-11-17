55775768e89fd5b64c212eadc0ce95dc38547204
Patrick Venture <venture@google.com>
2018-11-16 20:28:04 -0800

    lpc_handler: add initialized state
    The LPC handler mechanism requires an initialization step with input
    from the host.  Therefore, add a state to track whether this step has
    happened.
    
    Change-Id: I03f05df5fd6ae511836f324c866afcc9856ae8ab
    Signed-off-by: Patrick Venture <venture@google.com>

--- lpc_handler.cpp
+++ lpc_handler.cpp
@@ -18,7 +18,8 @@ bool LpcDataHandler::open()
 bool LpcDataHandler::close()
 {
     /* TODO: implement ioctl call to close window. */
-    return false;
+
+    return setInitializedAndReturn(false);
 }
 
 std::vector<std::uint8_t> LpcDataHandler::copyFrom(std::uint32_t length)
@@ -31,6 +32,14 @@ std::vector<std::uint8_t> LpcDataHandler::copyFrom(std::uint32_t length)
      * automatically read data, but rather must perform some ioctl or other
      * access to get the data from the driver.
      */
+    if (!initialized)
+    {
+        /* TODO: Consider designing some exceptions we can catch for when there
+         * is an error.
+         */
+        return {};
+    }
+
     return {};
 }
 
@@ -55,9 +64,10 @@ bool LpcDataHandler::write(const std::vector<std::uint8_t>& configuration)
     if (windowSize == 0)
     {
         /* Failed to map region. */
+        return false;
     }
 
-    return false;
+    return setInitializedAndReturn(true);
 }
 
 std::vector<std::uint8_t> LpcDataHandler::read()
