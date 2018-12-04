904a89972852b269c3bc0341d5a9dcc330b83eae
Patrick Venture <venture@google.com>
2018-12-03 16:24:26 -0800

    firmware: add logging to factory failures
    Add logging to factory failures.  This is now possible
    with injection available for phosphor-logging.
    
    Change-Id: I77ac959d58e44305d7acf918b656bdd21a9c4da8
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ test/firmware_createhandler_unittest.cpp
@@ -0,0 +1,45 @@
+#include "data_mock.hpp"
+#include "firmware_handler.hpp"
+
+#include <phosphor-logging/test/sdjournal_mock.hpp>
+#include <sdbusplus/test/sdbus_mock.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace phosphor::logging;
+
+namespace blobs
+{
+using ::testing::Return;
+using ::testing::StrEq;
+
+TEST(FirmwareHandlerBlobTest, VerifyFirmareCounts)
+{
+    /* Verify the firmware count must be greater than zero. */
+
+    DataHandlerMock dataMock;
+    SdJournalMock journalMock;
+    auto* old = SwapJouralImpl(&journalMock);
+
+    std::vector<HandlerPack> blobs;
+
+    std::vector<DataHandlerPack> data = {
+        {FirmwareBlobHandler::UpdateFlags::ipmi, nullptr},
+        {FirmwareBlobHandler::UpdateFlags::lpc, &dataMock},
+    };
+
+    sdbusplus::SdBusMock sdbus_mock;
+    auto bus_mock = sdbusplus::get_mocked_new(&sdbus_mock);
+
+    EXPECT_CALL(
+        journalMock,
+        journal_send_call(StrEq("Must provide at least one firmware handler.")))
+        .WillOnce(Return(0));
+
+    auto handler = FirmwareBlobHandler::CreateFirmwareBlobHandler(
+        std::move(bus_mock), blobs, data);
+
+    EXPECT_EQ(handler, nullptr);
+}
+
+} // namespace blobs
