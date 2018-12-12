6530a0b00208f48085f18fbaf07a30944aeb01f8
Patrick Venture <venture@google.com>
2018-12-12 13:12:21 -0800

    tools: blob: implement layer above ipmi
    Implement the layer above actual IPMI calls, such that we can verify the
    behavior.  There is a layer beneath this that'll compute CRCs before
    passing the commands down, that will go in next.
    
    Change-Id: I0b8e3aa93c171d829e32727c7bb1b411659d80bd
    Signed-off-by: Patrick Venture <venture@google.com>

--- tools/blob_handler.cpp
+++ tools/blob_handler.cpp
@@ -1,6 +1,45 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "blob_handler.hpp"
+
+int BlobHandler::getBlobCount()
+{
+    return 0;
+}
+
+std::string BlobHandler::enumerateBlob(int index)
+{
+    return "";
+}
 
 std::vector<std::string> BlobHandler::getBlobList()
 {
-    return {};
+    std::vector<std::string> list;
+    int blobCount = getBlobCount();
+
+    for (int i = 0; i < blobCount; i++)
+    {
+        auto name = enumerateBlob(i);
+        /* Currently ignore failures. */
+        if (!name.empty())
+        {
+            list.push_back(name);
+        }
+    }
+
+    return list;
 }
