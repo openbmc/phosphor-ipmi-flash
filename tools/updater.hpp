498714231b3ea9704e5d02370beb38166ff44922
Patrick Venture <venture@google.com>
2018-12-11 18:47:13 -0800

    tools: move interface building into main
    Move interface building into the main method to simplify processing and
    input further down the call stack.
    
    Change-Id: Ib14827bfe8c4ea89a04a7c201fdff9d538546ef5
    Signed-off-by: Patrick Venture <venture@google.com>

--- tools/updater.hpp
+++ tools/updater.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "blob_interface.hpp"
+#include "interface.hpp"
 
 #include <string>
 
@@ -8,10 +9,10 @@
  * Attempt to update the BMC's firmware using the interface provided.
  *
  * @param[in] blob - pointer to blob interface implementation object.
- * @param[in] interface - the interface to use.
+ * @param[in] handler - pointer to the data interface implementation object.
  * @param[in] imagePath - the path to the image file.
  * @param[in] signaturePath - the path to the signature file.
  * @return non-zero on failure.
  */
-int updaterMain(BlobInterface* blob, const std::string& interface,
+int updaterMain(BlobInterface* blob, DataInterface* handler,
                 const std::string& imagePath, const std::string& signaturePath);
