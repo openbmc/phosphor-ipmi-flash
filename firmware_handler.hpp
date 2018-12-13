3ca43622f901b87f4c17397b92dc023487ca3e4b
Patrick Venture <venture@google.com>
2018-12-13 09:15:11 -0800

    firmware: add read/write flags to enum
    Add read/write flags to header the tool can access.
    
    Note: Since this tool is meant to be run from the host and not the BMC,
    it's not 100% clear how it'll be built and installed properly in this
    configuration.
    
    Change-Id: Ifbeab169026b6651b815b87b3c1950e73f8dfaae
    Signed-off-by: Patrick Venture <venture@google.com>

--- firmware_handler.hpp
+++ firmware_handler.hpp
@@ -71,6 +71,8 @@ class FirmwareBlobHandler : public GenericBlobInterface
   public:
     enum UpdateFlags : std::uint16_t
     {
+        openRead = (1 << 0),  /* Flag for reading. */
+        openWrite = (1 << 1), /* Flag for writing. */
         ipmi = (1 << 8), /* Expect to send contents over IPMI BlockTransfer. */
         p2a = (1 << 9),  /* Expect to send contents over P2A bridge. */
         lpc = (1 << 10), /* Expect to send contents over LPC bridge. */
