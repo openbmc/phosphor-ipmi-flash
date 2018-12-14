ff35800c44a873eebb59bb537733270c2cfdc3f7
Patrick Venture <venture@google.com>
2018-12-13 16:55:23 -0800

    tools: add internal sys interface to ipmi
    Add internal sys interface to ipmi handler, s.t. the ipmi handler can do
    things such as open files, or call ioctls.
    
    Change-Id: I946cfcaed4d75f890f30d579f360688b0d4eb3cc
    Signed-off-by: Patrick Venture <venture@google.com>

--- tools/ipmi_handler.hpp
+++ tools/ipmi_handler.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "internal/sys.hpp"
 #include "ipmi_interface.hpp"
 
 namespace host_tool
@@ -8,10 +9,14 @@ namespace host_tool
 class IpmiHandler : public IpmiInterface
 {
   public:
-    IpmiHandler() = default;
+    explicit IpmiHandler(const internal::Sys* sys = &internal::sys_impl) :
+        sys(sys){};
 
     std::vector<std::uint8_t>
         sendPacket(const std::vector<std::uint8_t>& data) override;
+
+  private:
+    const internal::Sys* sys;
 };
 
 } // namespace host_tool
