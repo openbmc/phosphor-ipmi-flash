c1df9470c880019375f9b7b5bd0f592cfb8885eb
Patrick Venture <venture@google.com>
2018-12-14 13:57:28 -0800

    tools: implement method to send ipmi packet to BMC
    Implement method to send ipmi packets to BMC.  Only currently supports
    the system interface.
    
    Tested: Verified on a host that has the BLOB handler installed on the
    BMC, that it received a blob count of 0, as there were no handlers
    installed at that test.  Further functional tests will verify cases with
    the firmware handler installed, however, it is verified that this code
    correctly speaks IPMI to the BLOB handler registered.
    
    Change-Id: I696f3915930cc9fa5e768fbdeed484ea6cb707a2
    Signed-off-by: Patrick Venture <venture@google.com>

--- test/ipmi_interface_mock.hpp
+++ test/ipmi_interface_mock.hpp
@@ -12,7 +12,7 @@ class IpmiInterfaceMock : public IpmiInterface
   public:
     virtual ~IpmiInterfaceMock() = default;
     MOCK_METHOD1(sendPacket,
-                 std::vector<std::uint8_t>(const std::vector<std::uint8_t>&));
+                 std::vector<std::uint8_t>(std::vector<std::uint8_t>&));
 };
 
 } // namespace host_tool
