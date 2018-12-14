2e12a434dc53524126d43b7cf30383bc61020197
Patrick Venture <venture@google.com>
2018-12-13 18:05:11 -0800

    tools: ipmi handler must open ipmi device
    The ipmi handler must open the ipmi device on the host.
    
    Note: The initial implementation will only support ipmi system devices
    that are listed as device 0.  This can be improved later or swapped
    out to link against another ipmi command implementation.
    
    Change-Id: I262039fdf3554ba144be1943875fd088766f3b57
    Signed-off-by: Patrick Venture <venture@google.com>

--- tools/ipmi_handler.hpp
+++ tools/ipmi_handler.hpp
@@ -12,11 +12,28 @@ class IpmiHandler : public IpmiInterface
     explicit IpmiHandler(const internal::Sys* sys = &internal::sys_impl) :
         sys(sys){};
 
+    ~IpmiHandler() = default;
+    IpmiHandler(const IpmiHandler&) = delete;
+    IpmiHandler& operator=(const IpmiHandler&) = delete;
+    IpmiHandler(IpmiHandler&&) = default;
+    IpmiHandler& operator=(IpmiHandler&&) = default;
+
+    /**
+     * Attempt to open the device node.
+     *
+     * @return true on success, failure otherwise.
+     */
+    bool open();
+
     std::vector<std::uint8_t>
         sendPacket(const std::vector<std::uint8_t>& data) override;
 
   private:
     const internal::Sys* sys;
+    /** TODO: Use a smart file descriptor when it's ready.  Until then only
+     * allow moving this object.
+     */
+    int fd = -1;
 };
 
 } // namespace host_tool
