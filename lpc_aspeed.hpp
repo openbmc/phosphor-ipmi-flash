13586fd4e1943bfc942ff852a54296dfe1ecdc01
Patrick Venture <venture@google.com>
2018-12-14 09:44:02 -0800

    s/size_t/std::size_t/g - for compatibility with other builds
    For compatibility with the Google3 build system, use std::size_t instead
    of gnu size_t.
    
    Change-Id: Ie361de9c507119aa7db93d24b6e199547bd123af
    Signed-off-by: Patrick Venture <venture@google.com>

--- lpc_aspeed.hpp
+++ lpc_aspeed.hpp
@@ -12,9 +12,9 @@ class LpcMapperAspeed : public LpcMapperInterface
 {
   public:
     static std::unique_ptr<LpcMapperInterface>
-        createAspeedMapper(size_t regionSize);
+        createAspeedMapper(std::size_t regionSize);
 
-    LpcMapperAspeed(size_t regionSize,
+    LpcMapperAspeed(std::size_t regionSize,
                     const internal::Sys* sys = &internal::sys_impl) :
         regionSize(regionSize),
         sys(sys){};
@@ -23,7 +23,7 @@ class LpcMapperAspeed : public LpcMapperInterface
         mapWindow(std::uint32_t address, std::uint32_t length) override;
 
   private:
-    size_t regionSize;
+    std::size_t regionSize;
     const internal::Sys* sys;
 };
 
