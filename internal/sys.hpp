ac648cff45a3f680d8225f99d1e65f7e5b96320b
Patrick Venture <venture@google.com>
2018-12-14 09:44:02 -0800

    s/size_t/std::size_t/g - for compatibility with other builds
    For compatibility with the Google3 build system, use std::size_t instead
    of gnu size_t.
    
    Change-Id: Ie361de9c507119aa7db93d24b6e199547bd123af
    Signed-off-by: Patrick Venture <venture@google.com>

--- internal/sys.hpp
+++ internal/sys.hpp
@@ -26,9 +26,9 @@ class Sys
 
     virtual int open(const char* pathname, int flags) const = 0;
     virtual int close(int fd) const = 0;
-    virtual void* mmap(void* addr, size_t length, int prot, int flags, int fd,
-                       off_t offset) const = 0;
-    virtual int munmap(void* addr, size_t length) const = 0;
+    virtual void* mmap(void* addr, std::size_t length, int prot, int flags,
+                       int fd, off_t offset) const = 0;
+    virtual int munmap(void* addr, std::size_t length) const = 0;
     virtual int getpagesize() const = 0;
     virtual int ioctl(int fd, unsigned long request, void* param) const = 0;
 };
@@ -43,9 +43,9 @@ class SysImpl : public Sys
   public:
     int open(const char* pathname, int flags) const override;
     int close(int fd) const override;
-    void* mmap(void* addr, size_t length, int prot, int flags, int fd,
+    void* mmap(void* addr, std::size_t length, int prot, int flags, int fd,
                off_t offset) const override;
-    int munmap(void* addr, size_t length) const override;
+    int munmap(void* addr, std::size_t length) const override;
     int getpagesize() const override;
     int ioctl(int fd, unsigned long request, void* param) const override;
 };
