c7c416850041ae5b9f497f6771235fcc04b2c61e
Patrick Venture <venture@google.com>
2019-01-18 08:33:02 -0800

    tools: add io interface for mmio, etc
    Add an IO interface for host-side memory access.  This is done as an
    interface to allow platform specific implementations.
    
    Change-Id: Id8e35d5beb21f7db544e27d6592bbeac58748dcb
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ test/io_mock.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "io.hpp"
+
+#include <gmock/gmock.h>
+
+namespace host_tool
+{
+
+class HostIoInterfaceMock : public HostIoInterface
+{
+  public:
+    ~HostIoInterfaceMock() = default;
+
+    MOCK_METHOD3(write,
+                 bool(const std::size_t, const std::size_t, const void* const));
+};
+
+} // namespace host_tool
