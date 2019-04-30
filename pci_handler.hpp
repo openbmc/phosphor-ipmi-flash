92fbbdcc2bbe23567386f019fcdff344e4ca32a1
Patrick Venture <venture@google.com>
2019-04-30 16:48:19 -0700

    pci_handler: add aspeed-p2a-ctrl path
    Add the path to the aspeed-p2a-ctrl device for mapping access.
    
    The need to map, read, etc, is similar to lpc_handler, and they may
    converge.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I0b8e7bf1581f77abb819d10dba136569034c21e9

--- pci_handler.hpp
+++ pci_handler.hpp
@@ -4,6 +4,7 @@
 #include "internal/sys.hpp"
 
 #include <cstdint>
+#include <string>
 #include <vector>
 
 namespace blobs
@@ -35,6 +36,7 @@ class PciDataHandler : public DataInterface
   private:
     std::uint32_t regionAddress;
     const internal::Sys* sys;
+    static const std::string p2aControlPath;
 };
 
 } // namespace blobs
