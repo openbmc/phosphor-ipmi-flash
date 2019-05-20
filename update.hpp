8e801e182c881bc1ab5083dcb328496de1e2fcb3
Patrick Venture <venture@google.com>
2019-05-20 13:42:45 -0700

    bmc: add update mechanism interface
    Add mechanism definition for an update interface.  Does not provide an
    implementation.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I8d8f04538524ab42de19843844fef3eec6bbd140

---
+++ update.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "status.hpp"
+
+namespace ipmi_flash
+{
+
+class UpdateInterface
+{
+  public:
+    virtual ~UpdateInterface() = default;
+
+    /**
+     * Trigger the update mechanism.
+     *
+     * @return true if successfully started, false otherwise.
+     */
+    virtual bool triggerUpdate() = 0;
+
+    /** Abort the update process. */
+    virtual void abortUpdate() = 0;
+
+    /** Check the current state of the update process. */
+    virtual UpdateStatus status() = 0;
+};
+
+} // namespace ipmi_flash
