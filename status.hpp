63f5eac46edd0f47aa001d61d205a46cf67c9bd1
Patrick Venture <venture@google.com>
2019-05-20 13:42:45 -0700

    bmc: add update mechanism interface
    Add mechanism definition for an update interface.  Does not provide an
    implementation.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I8d8f04538524ab42de19843844fef3eec6bbd140

--- status.hpp
+++ status.hpp
@@ -12,4 +12,14 @@ enum class VerifyCheckResponses : std::uint8_t
     failed = 2,
     other = 3,
 };
-}
+
+/** The status of the update mechanism. */
+enum class UpdateStatus : std::uint8_t
+{
+    running = 0,
+    success = 1,
+    failed = 2,
+    unknown = 3,
+};
+
+} // namespace ipmi_flash
