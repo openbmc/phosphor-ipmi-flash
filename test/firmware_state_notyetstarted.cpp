a475f43911b8cfde472a1caa246c1b673229b59c
Patrick Venture <venture@google.com>
2019-05-23 08:40:21 -0700

    test: firmware notYetStarted: getBlobIds
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: Ia5593ae141d184314390faae3980a8331d2c3d7d

--- test/firmware_state_notyetstarted.cpp
+++ test/firmware_state_notyetstarted.cpp
@@ -14,5 +14,33 @@ class FirmwareHandlerNotYetStartedTest : public FakeLpcFirmwareTest
 {
 };
 
+/*
+ * There are the following calls (parameters may vary):
+ * Note: you cannot have a session yet, so only commands that don't take a
+ * session parameter are valid. Once you open() from this state, we will vary
+ * you transition out of this state (ensuring the above is true). Technically
+ * the firmware handler receives the session number with open(), but the blob
+ * manager is providing this normally.
+ *
+ * canHandleBlob
+ * getBlobIds
+ * deleteBlob
+ * stat
+ * open
+ *
+ * canHandleBlob is just a count check (or something similar) against what is
+ * returned by getBlobIds.  It is tested in firmware_canhandle_unittest
+ */
+
+TEST_F(FirmwareHandlerNotYetStartedTest, GetBlobListValidateListContents)
+{
+    /* TODO: Presently /flash/verify is present from the beginning, however,
+     * this is going to change to simplify handling of open().
+     */
+    std::vector<std::string> expectedBlobs = {};
+
+    EXPECT_THAT(handler->getBlobIds(), ContainerEq(expectedBlobs));
+}
+
 } // namespace ipmi_flashnamespace
 } // namespace ipmi_flash
