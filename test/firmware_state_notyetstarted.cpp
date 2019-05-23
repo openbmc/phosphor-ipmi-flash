167b9162d5070a984ab2b3f142ee81a28ec98804
Patrick Venture <venture@google.com>
2019-05-23 07:41:26 -0700

    test: start state-transition-specific unit-tests
    The goal of state-transition-specific unit-tests is to verify the
    commands at each state are accepted or rejected as expected.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: Ic1790ca01e298347afaf2162bba9cfdc6636b2dc

---
+++ test/firmware_state_notyetstarted.cpp
@@ -0,0 +1,15 @@
+/**
+ * The goal of these tests is to verify the behavior of all blob commands given the current state is notYetStarted.  The initial state.
+ */
+#include "firmware_handler.hpp"
+#include "firmware_unittest.hpp"
+
+#include <gtest/gtest.h>
+
+namespace ipmi_flash
+namespace {
+
+class FirmwareHandlerNotYetStartedTest : public FakeLpcFirmwareTest {};
+
+}
+} // namespace ipmi_flash
