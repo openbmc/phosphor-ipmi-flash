e312f39b98d0f6568f7ce7ac3d5f06087ca58de3
Patrick Venture <venture@google.com>
2019-06-04 07:44:37 -0700

    s/struct blobs::BlobMeta/blobs::BlobMeta/g
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I8f303e0c4a234cfe22b67170236a211564e37723

--- test/firmware_stat_unittest.cpp
+++ test/firmware_stat_unittest.cpp
@@ -32,7 +32,7 @@ TEST(FirmwareHandlerStatTest, StatOnInactiveBlobIDReturnsTransport)
     auto handler = FirmwareBlobHandler::CreateFirmwareBlobHandler(
         blobs, data, CreateVerifyMock(), CreateUpdateMock());
 
-    struct blobs::BlobMeta meta;
+    blobs::BlobMeta meta;
     EXPECT_TRUE(handler->stat("asdf", &meta));
     EXPECT_EQ(FirmwareBlobHandler::UpdateFlags::ipmi, meta.blobState);
 }
