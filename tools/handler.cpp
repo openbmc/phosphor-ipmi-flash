de73c3b744b0dad098cd46fecd84eab37ed066be
Patrick Venture <venture@google.com>
2019-06-20 19:38:17 -0700

    tools: bugfix: add missing close after failure
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: Id873c41997e2617d1fa3db5767112c23d77c487c

--- tools/handler.cpp
+++ tools/handler.cpp
@@ -130,6 +130,7 @@ bool UpdateHandler::verifyFile(const std::string& target)
     }
     catch (const ipmiblob::BlobException& b)
     {
+        blob->closeBlob(session);
         throw ToolException("blob exception received: " +
                             std::string(b.what()));
     }
@@ -164,14 +165,23 @@ void UpdateHandler::cleanArtifacts()
         session =
             blob->openBlob(ipmi_flash::cleanupBlobId,
                            static_cast<std::uint16_t>(blobs::OpenFlags::write));
+    }
+    catch (...)
+    {
+        return;
+    }
+
+    try
+    {
         std::fprintf(stderr, "Committing to the cleanup blob\n");
         blob->commit(session, {});
         std::fprintf(stderr, "Closing cleanup blob\n");
-        blob->closeBlob(session);
     }
     catch (...)
     {
     }
+
+    blob->closeBlob(session);
 }
 
 } // namespace host_tool
