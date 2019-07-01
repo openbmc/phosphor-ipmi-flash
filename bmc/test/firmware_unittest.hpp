06874336e6d86f837f91bdc759be4683c1a2af26
Patrick Venture <venture@google.com>
2019-07-01 09:22:38 -0700

    bmc: add ActionPack notion to bundle actions
    Each firmware type will provide its own set of action implementations
    for each step, preparation, verification, and update.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: Id6409ac356a74e9094272b37709861e2a33d9862

--- bmc/test/firmware_unittest.hpp
+++ bmc/test/firmware_unittest.hpp
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <gmock/gmock.h>
@@ -42,9 +43,16 @@ class IpmiOnlyFirmwareStaticTest : public ::testing::Test
             std::make_unique<TriggerMock>();
         updateMockPtr = reinterpret_cast<TriggerMock*>(updateMock.get());
 
+        std::unique_ptr<ActionPack> actionPack = std::make_unique<ActionPack>();
+        actionPack.preparation = std::move(prepareMock);
+        actionPack.verification = std::move(verifyMock);
+        actionPack.update = std::move(updateMock);
+
+        ActionMap packs;
+        packs[staticLayoutBlobId] = std::move(actionPack);
+
         handler = FirmwareBlobHandler::CreateFirmwareBlobHandler(
-            blobs, data, std::move(prepareMock), std::move(verifyMock),
-            std::move(updateMock));
+            blobs, data, std::move(packs));
     }
 
     void expectedState(FirmwareBlobHandler::UpdateState state)
