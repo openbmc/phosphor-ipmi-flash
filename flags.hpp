545f5658305a807dbfe0105a62a8b0cfe42e1808
Benjamin Fair <benjaminfair@google.com>
2019-10-09 14:18:54 -0700

    bmc: add network bridge support
    This data handler listens on a TCP port for the image bytes.
    
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>
    Change-Id: I388fdd7303c5f8bc93a8f75e97b3abf8adbb81af

--- flags.hpp
+++ flags.hpp
@@ -15,6 +15,9 @@ class FirmwareFlags
         ipmi = (1 << 8), /* Expect to send contents over IPMI BlockTransfer. */
         p2a = (1 << 9),  /* Expect to send contents over P2A bridge. */
         lpc = (1 << 10), /* Expect to send contents over LPC bridge. */
+        /* New bridges starting with net densely pack the rest of the bits */
+        net = (1 << 11), /* Expect to send contents over network bridge. */
+        /* nextBridge = (2 << 11) */
     };
 };
 
