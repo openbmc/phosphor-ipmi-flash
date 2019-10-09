59c430784cca0af62f842bac5d7c0a3e897bb9b0
Benjamin Fair <benjaminfair@google.com>
2019-10-09 14:18:54 -0700

    bmc: add network bridge support
    This data handler listens on a TCP port for the image bytes.
    
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>
    Change-Id: I388fdd7303c5f8bc93a8f75e97b3abf8adbb81af

--- flags.hpp
+++ flags.hpp
@@ -10,11 +10,12 @@ class FirmwareFlags
   public:
     enum UpdateFlags : std::uint16_t
     {
-        openRead = (1 << 0),  /* Flag for reading. */
-        openWrite = (1 << 1), /* Flag for writing. */
-        ipmi = (1 << 8), /* Expect to send contents over IPMI BlockTransfer. */
-        p2a = (1 << 9),  /* Expect to send contents over P2A bridge. */
-        lpc = (1 << 10), /* Expect to send contents over LPC bridge. */
+        openRead = (1 << 0),  // Flag for reading
+        openWrite = (1 << 1), // Flag for writing
+        ipmi = (0b001 << 8),  // Expect to send contents over IPMI BlockTransfer
+        net = (0b011 << 8),   // Expect to send contents over network bridge
+        p2a = (0b010 << 8),   // Expect to send contents over P2A bridge
+        lpc = (0b100 << 8),   // Expect to send contents over LPC bridge
     };
 };
 
