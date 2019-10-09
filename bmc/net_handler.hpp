a9de78808add40e82c74e7a6ed65ecfa732afe52
Benjamin Fair <benjaminfair@google.com>
2019-10-09 14:18:54 -0700

    bmc: add network bridge support
    This data handler listens on a TCP port for the image bytes.
    
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>
    Change-Id: I388fdd7303c5f8bc93a8f75e97b3abf8adbb81af

---
+++ bmc/net_handler.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "data_handler.hpp"
+
+#include <unistd.h>
+
+#include <cstdint>
+#include <stdplus/handle/managed.hpp>
+#include <vector>
+
+namespace ipmi_flash
+{
+
+/**
+ * Data Handler for receiving the image over a network port
+ */
+class NetDataHandler : public DataInterface
+{
+  public:
+    NetDataHandler() : listenFd(std::nullopt), connFd(std::nullopt)
+    {
+    }
+
+    bool open() override;
+    bool close() override;
+    std::vector<std::uint8_t> copyFrom(std::uint32_t length) override;
+    bool writeMeta(const std::vector<std::uint8_t>& configuration) override;
+    std::vector<std::uint8_t> readMeta() override;
+
+    static constexpr int listenPort = 622;
+
+  private:
+    static void closefd(int&& fd)
+    {
+        ::close(fd);
+    }
+    using Fd = stdplus::Managed<int>::Handle<closefd>;
+
+    Fd listenFd;
+    Fd connFd;
+};
+
+} // namespace ipmi_flash
