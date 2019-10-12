12cdf5802aebe2fc42e5f8a0c2a2c1d51abab76a
Benjamin Fair <benjaminfair@google.com>
2019-10-11 17:55:27 -0700

    sys: add networking functions
    Add networking related functions to the syscall wrapper.
    
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>
    Change-Id: I0ccb4138963ce2a8c7bb6f6f3a556de1afcd0c08

--- tools/test/internal_sys_mock.hpp
+++ tools/test/internal_sys_mock.hpp
@@ -26,6 +26,13 @@ class InternalSysMock : public Sys
     MOCK_CONST_METHOD0(getpagesize, int());
     MOCK_CONST_METHOD3(ioctl, int(int, unsigned long, void*));
     MOCK_CONST_METHOD3(poll, int(struct pollfd*, nfds_t, int));
+    MOCK_CONST_METHOD3(socket, int(int, int, int));
+    MOCK_CONST_METHOD3(connect, int(int, const struct sockaddr*, socklen_t));
+    MOCK_CONST_METHOD4(sendfile, ssize_t(int, int, off_t*, size_t));
+    MOCK_CONST_METHOD4(getaddrinfo,
+                       int(const char*, const char*, const struct addrinfo*,
+                           struct addrinfo**));
+    MOCK_CONST_METHOD1(freeaddrinfo, void(struct addrinfo*));
     MOCK_CONST_METHOD1(getSize, std::int64_t(const char*));
 };
 
