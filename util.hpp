ede9c9f6684e7d8367f039c48839e464d3d34eaf
Patrick Venture <venture@google.com>
2020-09-30 13:49:19 -0700

    util: use constexpr string instead of string objects
    This handles an initialization order issue found with address
    santization testing.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I7b3b6e980e378855e8bda8209dba18c20b67a000

--- util.hpp
+++ util.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
-#include <string>
-
 namespace ipmi_flash
 {
 
-extern const std::string biosBlobId;
-extern const std::string updateBlobId;
-extern const std::string verifyBlobId;
-extern const std::string hashBlobId;
-extern const std::string activeImageBlobId;
-extern const std::string activeHashBlobId;
-extern const std::string staticLayoutBlobId;
-extern const std::string ubiTarballBlobId;
-extern const std::string cleanupBlobId;
+inline constexpr char biosBlobId[] = "/flash/bios";
+inline constexpr char updateBlobId[] = "/flash/update";
+inline constexpr char verifyBlobId[] = "/flash/verify";
+inline constexpr char hashBlobId[] = "/flash/hash";
+inline constexpr char activeImageBlobId[] = "/flash/active/image";
+inline constexpr char activeHashBlobId[] = "/flash/active/hash";
+inline constexpr char staticLayoutBlobId[] = "/flash/image";
+inline constexpr char ubiTarballBlobId[] = "/flash/tarball";
+inline constexpr char cleanupBlobId[] = "/flash/cleanup";
 
 } // namespace ipmi_flash
