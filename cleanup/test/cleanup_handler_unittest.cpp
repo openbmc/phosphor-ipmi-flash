48474bc1d98067d15074002fc32929f488155fd6
Patrick Venture <venture@google.com>
2020-10-05 09:38:33 -0700

    cleanup: coverage: add missing coverage
    The uninteresting blob methods did not receive unit-tests. This adds
    those missing unit-tests.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: Ib5885e3f3fa9e4e708fbf578bcaaa8bc85ff3993

--- cleanup/test/cleanup_handler_unittest.cpp
+++ cleanup/test/cleanup_handler_unittest.cpp
@@ -2,6 +2,9 @@
 #include "filesystem_mock.hpp"
 #include "util.hpp"
 
+#include <blobs-ipmid/blobs.hpp>
+
+#include <cstdint>
 #include <memory>
 #include <string>
 #include <vector>
@@ -45,6 +48,25 @@ TEST_F(CleanupHandlerTest, CommitShouldDeleteFiles)
     EXPECT_CALL(*mock_ptr, remove("efgh")).WillOnce(Return());
 
     EXPECT_TRUE(handler->commit(1, {}));
+}
+
+TEST_F(CleanupHandlerTest, VerifyDefaultBlobMethods)
+{
+    // Test each of the blob handler commands.
+    EXPECT_TRUE(handler->open(/*session*/ 0, /*flags*/ 0, "abcd"));
+    EXPECT_TRUE(handler->close(/*session*/ 0));
+    EXPECT_TRUE(handler->expire(/*session*/ 0));
+    EXPECT_FALSE(handler->deleteBlob("abcd"));
+
+    blobs::BlobMeta meta;
+    EXPECT_FALSE(handler->stat("abcd", &meta));
+    EXPECT_FALSE(handler->stat(/*session*/ 0, &meta));
+
+    EXPECT_THAT(handler->read(/*session*/ 0, /*offset*/ 0, 1),
+                ::testing::IsEmpty());
+
+    std::vector<uint8_t> data = {0x01};
+    EXPECT_FALSE(handler->write(/*session*/ 0, /*offset*/ 0, data));
 }
 
 } // namespace
