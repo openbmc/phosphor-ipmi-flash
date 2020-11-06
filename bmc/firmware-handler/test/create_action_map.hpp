b45da42e8bf817110b3ff3b0424bfd4fc2e89937
Jason Ling <jasonling@google.com>
2020-11-06 13:45:09 -0800

    test: move shared mocks to common area
    Move mocks that can be shared between version and firmware handler tests
    to parent directory (bmc/test)
    
    This will eliminate the need to duplicate the definition of testing
    mocks
    
    Signed-off-by: Jason Ling <jasonling@google.com>
    Change-Id: Id41c56271d0bfbcb8c18da1f903c7786699508d4

--- bmc/firmware-handler/test/create_action_map.hpp
+++ bmc/firmware-handler/test/create_action_map.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "firmware_handler.hpp"
-#include "status.hpp"
+#include "triggerable_mock.hpp"
 
 #include <memory>
 #include <string>
@@ -11,21 +11,6 @@
 
 namespace ipmi_flash
 {
-// TriggerableActionInterface
-
-class TriggerMock : public TriggerableActionInterface
-{
-  public:
-    MOCK_METHOD0(trigger, bool());
-    MOCK_METHOD0(abort, void());
-    MOCK_METHOD0(status, ActionStatus());
-};
-
-std::unique_ptr<TriggerableActionInterface> CreateTriggerMock()
-{
-    return std::make_unique<TriggerMock>();
-}
-
 ActionMap CreateActionMap(const std::string& blobPath)
 {
     std::unique_ptr<ActionPack> actionPack = std::make_unique<ActionPack>();
