84904b97d4952a4f48ace53db67d314efcb02be5
William A. Kennington III <wak@google.com>
2021-01-21 23:48:50 -0800

    version-handler/test: Remove unused declarations
    Change-Id: If9bc7dd4c58bd9ba3430ffee6319da5ed194cb9f
    Signed-off-by: William A. Kennington III <wak@google.com>

--- bmc/version-handler/test/version_close_unittest.cpp
+++ bmc/version-handler/test/version_close_unittest.cpp
@@ -7,7 +7,6 @@
 
 #include <gtest/gtest.h>
 
-using ::testing::_;
 using ::testing::Return;
 
 namespace ipmi_flash
--- bmc/version-handler/test/version_open_unittest.cpp
+++ bmc/version-handler/test/version_open_unittest.cpp
@@ -8,7 +8,6 @@
 
 #include <gtest/gtest.h>
 
-using ::testing::_;
 using ::testing::Return;
 
 namespace ipmi_flash
