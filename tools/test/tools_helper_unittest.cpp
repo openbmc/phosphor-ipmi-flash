8c39ac13f094a9f08babfbe96d892e7f6fc903ac
William A. Kennington III <wak@google.com>
2021-10-13 21:40:11 -0700

    tools: Support more frequent status checks
    This changes the check logic to query the BMC more frequently at the
    beginning and exponentially back off if the BMC is not yet ready. This
    makes short updates significantly faster.
    
    Change-Id: Icfcc1d0c9ef20aa04de754bfe2da04add6ee51e6
    Signed-off-by: William A. Kennington III <wak@google.com>

--- tools/test/tools_helper_unittest.cpp
+++ tools/test/tools_helper_unittest.cpp
@@ -1,5 +1,6 @@
 #include "helper.hpp"
 #include "status.hpp"
+#include "tool_errors.hpp"
 
 #include <blobs-ipmid/blobs.hpp>
 #include <ipmiblob/test/blob_interface_mock.hpp>
@@ -43,7 +44,7 @@ TEST_F(HelperTest, PollStatusReturnsAfterFailure)
     EXPECT_CALL(blobMock, getStat(TypedEq<std::uint16_t>(session)))
         .WillOnce(Return(verificationResponse));
 
-    EXPECT_FALSE(pollStatus(session, &blobMock));
+    EXPECT_THROW(pollStatus(session, &blobMock), ToolException);
 }
 
 TEST_F(HelperTest, PollReadReadyReturnsAfterSuccess)
@@ -68,7 +69,7 @@ TEST_F(HelperTest, PollReadReadyReturnsAfterFailure)
     EXPECT_CALL(blobMock, getStat(TypedEq<std::uint16_t>(session)))
         .WillOnce(Return(blobResponse));
 
-    EXPECT_FALSE(pollReadReady(session, &blobMock).first);
+    EXPECT_THROW(pollReadReady(session, &blobMock).first, ToolException);
 }
 
 TEST_F(HelperTest, PollReadReadyReturnsAfterRetrySuccess)
@@ -102,7 +103,7 @@ TEST_F(HelperTest, PollReadReadyReturnsAfterRetryFailure)
         .WillOnce(Return(blobResponseRunning))
         .WillOnce(Return(blobResponseError));
 
-    EXPECT_FALSE(pollReadReady(session, &blobMock).first);
+    EXPECT_THROW(pollReadReady(session, &blobMock).first, ToolException);
 }
 
 TEST_F(HelperTest, MemcpyAlignedOneByte)
