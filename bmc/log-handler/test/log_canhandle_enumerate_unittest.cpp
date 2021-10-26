72cd28153555f254ca27fcd4a4dfc5ad50cc3634
Gaurav Gandhi <gauravgandhi@google.com>
2021-10-26 20:43:25 +0000

    Add support for log-handler
    Signed-off-by: Gaurav Gandhi <gauravgandhi@google.com>
    Change-Id: I9ef775af752156a1647453ff3831ef4c0449d546

---
+++ bmc/log-handler/test/log_canhandle_enumerate_unittest.cpp
@@ -0,0 +1,29 @@
+#include "log_handler.hpp"
+#include "log_mock.hpp"
+
+#include <array>
+
+#include <gtest/gtest.h>
+
+namespace ipmi_flash
+{
+
+TEST(LogHandlerCanHandleTest, VerifyGoodInfoMap)
+{
+    constexpr std::array blobNames{"blob0", "blob1", "blob2", "blob3"};
+    LogBlobHandler handler(createMockLogConfigs(blobNames));
+    for (const auto& blobName : blobNames)
+    {
+        EXPECT_TRUE(handler.canHandleBlob(blobName));
+    }
+}
+
+TEST(LogHandlerEnumerateTest, VerifyGoodInfoMap)
+{
+    constexpr std::array blobNames{"blob0", "blob1", "blob2", "blob3"};
+    LogBlobHandler handler(createMockLogConfigs(blobNames));
+    EXPECT_THAT(handler.getBlobIds(),
+                ::testing::UnorderedElementsAreArray(blobNames));
+}
+
+} // namespace ipmi_flash
